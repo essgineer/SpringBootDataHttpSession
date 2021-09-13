cb14bdc6d52306604e2d0c4e91c0952a9371c8d2
Sqqal <Sqqal@192.168.6.238>
2021-09-13 18:44:31 +0200

    no comment

---
+++ SpringBootClientDataBrowser/src/main/java/com/essgineer/demo/SpringBootClientDataBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.essgineer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientDataBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientDataBrowserApplication.class, args);
+	}
+
+}
